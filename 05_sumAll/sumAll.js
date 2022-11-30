a87c90f4185173d9aae8a55e9b0c197166726a0f
Tianwen <skyyang0202@gmail.com>
2022-11-30 14:44:53 +0800

    finished ex05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,20 @@
-const sumAll = function() {
-
+const sumAll = function(a, b) {
+    let sum = 0;
+    if (a>0 && b>0 && typeof a === "number" && typeof b === "number"){
+        if (a<b) {
+            for (let x=a; x<=b; x++){
+                sum += x;
+            }
+            return sum;
+        } else {
+            for (let x=b; x<=a; x++){
+                sum += x;
+            }
+            return sum;
+        }
+    } else {
+        return "ERROR";
+    }
 };
 
 // Do not edit below this line
